dea507685eab031963af2739524ea06c19c4aaef
tonyolyva <olyvatony@gmail.com>
2025-03-24 23:30:38 -0700

    Initial Commit

---
+++ InventoryServ/InventoryServApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryServApp.swift
+//  InventoryServ
+//
+//  Created by Anatoliy Olyva on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct InventoryServApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
