2e231c82c97004fa3334dca49c19c7f6eac587ed
tonyolyva <olyvatony@gmail.com>
2025-03-24 23:36:00 -0700

    init the proj

--- InventoryServ/ContentView.swift
+++ InventoryServ/ContentView.swift
@@ -2,20 +2,30 @@
 //  ContentView.swift
 //  InventoryServ
 //
-//  Created by Anatoliy Olyva on 3/24/25.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    var items: [Item] = [
+        Item(name: "USB Cable", description: "Long USB-C cable", location: "Row 1, Box 5", image: "usb_cable", category: "Electronics"),
+        Item(name: "Adapter", description: "USB-C to HDMI adapter", location: "Row 2, Box 8", image: "adapter", category: "Electronics"),
+        Item(name: "Hammer", description: "Heavy hammer", location: "Row 3, Box 1", image: "hammer", category: "Tools"),
+        // Add more items...
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(items) { item in
+                VStack(alignment: .leading) {
+                    Text(item.name).font(.headline)
+                    Text(item.location).font(.subheadline)
+                    if let description = item.description{
+                        Text(description).font(.caption)
+                    }
+                }
+            }
+            .navigationTitle("Inventory")
         }
-        .padding()
     }
 }
 
